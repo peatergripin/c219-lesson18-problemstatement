edfed741fc5640f9ecc9097acce3dd47bced8c21
Irfan bin Imran <peatergripin@gmail.com>
2026-01-17 18:22:33 +0800

    remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import "../App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Diplomas from "./Diplomas";
